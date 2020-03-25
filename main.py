18a37553a777ee30be25717c22e3bd52dbf3e960
José Maia <josecbmaia@outlook.pt>
2020-03-25 14:38:22 +0000

    Add test

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import csv
+import io
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    #my_input = os.environ["INPUT_MYINPUT"]
+    my_input = "Jorge"
 
-    my_output = f"Hello {my_input}"
+    outBuffer = io.StringIO()
+    #fields = ['Name', 'Sex', 'Age', 'Height (in)', 'Weight (lbs)']
+    with open('sampleFiles/a.csv', 'r') as csvfile:
+        reader = csv.reader(
+            csvfile, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
+        writer = csv.writer(
+            outBuffer, quoting=csv.QUOTE_NONNUMERIC, skipinitialspace=True)
+        for row in reader:
+            writer.writerow(row)
 
-    print(f"::set-output name=myOutput::{my_output}")
+    # Add new lines
+    writer.writerow([my_input, 'M', 25.0, 128.0, 205.0])
+    print(f"::set-output name=myOutput::{outBuffer.getvalue()}")
+    outBuffer.close()
 
 
 if __name__ == "__main__":
